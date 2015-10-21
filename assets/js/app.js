489a3538e496e94d8d5a2104c13751ed843435c9
Peter Black <sonicblend@gmail.com>
2015-10-21 02:31:58 +0100

    move app start logic out of index.html and into app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,37 @@ Music.on("before:start", function(){
 
     Music.regions = new RegionContainer();
 });
+
+//    Music.StaticView = Marionette.ItemView.extend({
+//        template: "#results-empty"
+//    }
+
+//    Music.ResultView = Marionette.ItemView.extend({
+//        template: "#results-open"
+//    });
+
+Music.on("start", function(){
+//        var eska = new Music.Result({
+//            query: "ESKA - Eska",
+//            deezer: {
+//                artist: "Eska",
+//                title: "Eska",
+//                link: "http://www.deezer.com/album/9535910",
+//                id: "9535910",
+//            },
+//            bandcamp: {
+//                artist: "ESKA",
+//                title: "ESKA",
+//                link: "https://eskaonline.bandcamp.com/album/eska",
+//                id: "350367951",
+//            },
+//        });
+
+    Music.HistoryApp.List.Controller.listHistory();
+
+//        var eskaView = new Music.ResultView({
+//            model: eska
+//        });
+//        var staticView = new Music.StaticView();
+//        Music.regions.main.show(staticView);
+});
