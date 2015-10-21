356b03d8cf15c8b4a6ddde4138511621e83a6228
Peter Black <sonicblend@gmail.com>
2015-10-21 09:58:47 +0100

    display history as a CompositeView to include the results heading text. fix unclosed paragraph in results-header template.

--- assets/js/apps/history/list/list_view.js
+++ assets/js/apps/history/list/list_view.js
@@ -4,9 +4,13 @@ Music.module("HistoryApp.List", function(List, Music, Backbone, Marionette, $, _
         template: "#results-closed",
     });
 
-    List.Results = Marionette.CollectionView.extend({
+    // use CompositeView rather than CollectionView, to additionally display
+    // the #results-header template
+    List.Results = Marionette.CompositeView.extend({
         tagName: "ol",
         className: "list-unstyled",
+        template: "#results-header",
         childView: List.Result,
+        childViewContainer: "#history",
     });
 });
